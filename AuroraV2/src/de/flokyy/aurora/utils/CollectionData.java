1168568bd8f0d70cf15d0796d8726b1b6b84bcb8
Flokyy <68162827+Flokyyy@users.noreply.github.com>
2022-12-08 00:35:30 +0100

    Update CollectionData.java

--- AuroraV2/src/de/flokyy/aurora/utils/CollectionData.java
+++ AuroraV2/src/de/flokyy/aurora/utils/CollectionData.java
@@ -1,7 +1,10 @@
 package de.flokyy.aurora.utils;
 
+import java.awt.Color;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.OffsetDateTime;
 import java.util.concurrent.TimeUnit;
 
 import org.json.simple.JSONArray;
@@ -11,7 +14,9 @@ import org.json.simple.parser.JSONParser;
 import de.flokyy.aurora.Aurora;
 import de.flokyy.aurora.mysql.MySQLStatements;
 import de.flokyy.aurora.solana.UpdateMetadata;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -24,6 +29,21 @@ public class CollectionData {
 		   return bd.doubleValue();
 	}
 
+	public static String updateMetadata(String token, boolean originalMetadata, String oldURI, int tries) {
+		if(tries != 4) {
+		UpdateMetadata check = new UpdateMetadata(token); //Updating the metadata back to the original one
+		String update = check.run(token, originalMetadata, oldURI);
+			
+		if(update.equalsIgnoreCase("ERROR")) { //Couldn't update
+			updateMetadata(token, originalMetadata, oldURI, tries + 1);
+		}
+		else {
+			return update;
+			  }
+		}
+		return "ERROR";
+	}
+		
 	public static void getLatestData() {
 		try {
 			System.out.println("Fetching data from coralcube...");
@@ -125,46 +145,37 @@ public class CollectionData {
        			
 				}
 	       		try { 
-	       			UpdateMetadata check = new UpdateMetadata(mint); // Else we are updating the metadata
-					String update = check.run(mint, false, "EMPTY");
-					
-					if(!MySQLStatements.cacheTransactionExists(signature)) { // Checking if signature wasn't saved already
-						Aurora.mysql.update("INSERT INTO auroraCache(TRANSACTION) VALUES ('" + signature + "')"); //Saving data
+	       			
+	       			if(!MySQLStatements.cacheTransactionExists(signature)) { // Checking if signature wasn't saved already
+					Aurora.mysql.update("INSERT INTO auroraCache(TRANSACTION) VALUES ('" + signature + "')"); //Saving data
 			       		Aurora.mysql.update("UPDATE auroraCache SET TOKEN='" + mint + "'WHERE TRANSACTION='" + signature + "'");
 			       		Aurora.mysql.update("UPDATE auroraCache SET PAID_ROYALTY='" + 0.0 + "'WHERE TRANSACTION='" + signature + "'");
 			       		Aurora.mysql.update("UPDATE auroraCache SET SALE_PRICE='" + nftPrice + "'WHERE TRANSACTION='" + signature + "'");
 			       		Aurora.mysql.update("UPDATE auroraCache SET OWED_ROYALTY='" + owedRoyalty + "'WHERE TRANSACTION='" + signature + "'");
 			       		Aurora.mysql.update("UPDATE auroraCache SET OLD_URI='" + updatedURI + "'WHERE TRANSACTION='" + signature + "'"); // Will be used for later usage
 					}
-					
-					if(update.equalsIgnoreCase("ERROR")) { //Couldn't update
-						
-			    		System.out.println("Error when trying to lock metadata from token: " + mint + "... trying again!");
-			       		UpdateMetadata check1 = new UpdateMetadata(mint); //Trying to update the metadata again
-
-						String update1 = check1.run(mint, false, "EMPTY");
-						if(!update1.equalsIgnoreCase("ERROR")) {
-							System.out.println("Success!");	
-						}
-						else {
-							System.out.println("Transaction failed again!");	
-						}
-	       				
-					}
-					else {
-						
-					if(!MySQLStatements.metadataAlreadyChanged(signature)) {	
-					   MySQLStatements.metaDataSaved(signature, mint, updatedURI);
-					}
-		       		System.out.println("Successfully updated metadata for: " + mint);
-		       		System.out.println("Saved new entry with transaction: " + signature);
-		       		
-		       		
-//		       		TextChannel tc = Aurora.jda.getTextChannelById(1049039083806150686L);
-//		       		tc.sendMessage("New Orbs got locked: TOKEN: " + mint + " | PRICE: " + nftPrice + " SOL | " +  "TRANSACTION: " + signature).queue();
-					continue;
+	       			
+	       			String update = null;
+					try {
+						update = updateMetadata(mint, false, "EMTPY", 1);
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
 					}
 					
+					if(!update.equalsIgnoreCase("ERROR") && !(update != null)) { //Successfully updated the metadata to a locked NFT
+						
+						if(!MySQLStatements.metadataAlreadyChanged(signature)) {	
+							   MySQLStatements.metaDataSaved(signature, mint, updatedURI);
+							}
+				       		System.out.println("Successfully updated metadata for: " + mint);
+				       		System.out.println("Saved new entry with transaction: " + signature);
+						}
+					
+					
+					else { 
+						System.out.println("Error when trying to lock metadata from token: " + mint + "...");
+					}
 	       			}
 	       			catch(Exception e) { 
 	       				continue;
