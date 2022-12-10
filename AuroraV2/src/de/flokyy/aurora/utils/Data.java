6607f656a3f550c2c6ba8e8d2713d2910472a5d4
Flokyy <68162827+Flokyyy@users.noreply.github.com>
2022-12-10 22:24:06 +0100

    Update Data.java

--- AuroraV2/src/de/flokyy/aurora/utils/Data.java
+++ AuroraV2/src/de/flokyy/aurora/utils/Data.java
@@ -8,5 +8,5 @@ public class Data {
 	public static double creator_royalty = 0.07; // Royalty percentage that is being charged
 	public static String vault_wallet = "yoursolanavaultwallet";
 	public static String default_UriLink = "https://cdn.discordapp.com/attachments/1048663249488453693/1048664594178121820/locked.json"; //Enter your lock uri file here. MUST BE A .json. This is the default URI
-	
+	public static boolean dynamicRoyalty = false; //Set this to true if dynamic royalties should be enabled.
 }
